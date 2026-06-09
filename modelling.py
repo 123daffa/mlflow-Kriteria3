a09c0a9a006c4d51c2788020e76d2db510d2760f
daffanur19 <daffanur19@gmail.com>
2026-06-09 11:54:27 +0700

    update modelling

--- modelling.py
+++ modelling.py
@@ -4,52 +4,48 @@ import mlflow
 from sklearn.linear_model import SGDClassifier
 from joblib import dump
 
-
 # Memuat dataset
 data = pd.read_csv("wine.csv")
 
-# Set MLflow Tracking URI
-mlflow.set_tracking_uri("file:./mlruns")
+# Model Online Learning
+model = SGDClassifier(
+    loss='log_loss',
+    learning_rate='adaptive',
+    eta0=0.01,
+    max_iter=10000
+)
 
-
-# Create a new MLflow Experiment
-mlflow.set_experiment("Online Training Wine")
-
-
-# Model Online Learning: SGDClassifier
-model = SGDClassifier(loss='log_loss', learning_rate='adaptive', eta0=0.01, max_iter=10000)
-
-
-# Kelas target (diperlukan untuk partial_fit)
+# Kelas target
 classes = data['target'].unique()
 
+# Aktifkan autolog
+mlflow.autolog()
 
-with mlflow.start_run():
-    mlflow.autolog()  # Melakukan logging otomatis
+# Preprocessing data
+X_batch = data.drop(columns=['target'])
+y_batch = data['target']
 
+# Training
+model.partial_fit(X_batch, y_batch, classes=classes)
 
-    # Preprocessing data untuk batch
-    X_batch = data.drop(columns=['target'])
-    y_batch = data['target']
+# Evaluasi
+accuracy = model.score(X_batch, y_batch)
+mlflow.log_metric("accuracy", accuracy)
 
+print(f"Accuracy: {accuracy:.4f}")
 
-    # Initial fit untuk batch pertama
-    model.partial_fit(X_batch, y_batch, classes=classes)
+# Simpan model
+dump(model, "online_model.joblib")
 
+# Log artifact
+mlflow.log_artifact(
+    "online_model.joblib",
+    artifact_path="model_artifacts"
+)
 
-    # Log metrik setelah setiap batch
-    accuracy = model.score(X_batch, y_batch)
-    mlflow.log_metric("accuracy", accuracy)
-    print(f"Accuracy: {accuracy:.4f}")
-    # Simpan model ke file lokal
-    dump(model, "online_model.joblib")
-
-    # Log file model sebagai artefak ke MLflow
-    mlflow.log_artifact("online_model.joblib", artifact_path="model_artifacts")
-
-    # Log model setelah selesai online training
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="online_model",
-        input_example=X_batch.iloc[:5]
-    )
+# Log model
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="online_model",
+    input_example=X_batch.iloc[:5]
+)
